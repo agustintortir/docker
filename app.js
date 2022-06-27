c6ba61e9408c84c8a72b34bc9d90fc989f30f2d2
agustintortir <agustinipm16@gmail.com>
2022-06-27 15:29:49 -0300

    hashfinal

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hola mundo <br>" + "/*******  HASH_ID *******/" + "/*******  HASH_DATE *******/" )
+  res.send("Hola mundo <br>" + "/*******  HASH_DATE *******/" + "_" + "/*******  HASH_ID *******/" )
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-}); "<aaaaaaaaaaaaaaaaaaaaaaaestrooiaarjoihoisdasdasdrougrhbgr"
-
+});
