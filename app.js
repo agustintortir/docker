af53145dacd27384586066aeedfb5b615638c841
agustintortir <agustinipm16@gmail.com>
2022-06-27 15:14:56 -0300

    hashydate

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hola mundo <br>" + "/*******  HASH_ID *******/" )
+  res.send("Hola mundo <br>" + "/*******  HASH_ID *******/" + "/*******  HASH_DATE *******/" )
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
