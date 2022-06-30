dd88815dbe783f0e0b7ca363af51f6efe60e79f6
agustintortir <agustinipm16@gmail.com>
2022-06-30 15:27:07 -0300

    supertest

--- tests/ejemplo.test.js
+++ tests/ejemplo.test.js
@@ -1,7 +1,11 @@
+const app = require("../app.js");
+const request = require("supertest")(app);
+
 describe("Test ejemplo", () => {
   describe("Test ejemplo", () => {
-    test("Test ejemplo", () => {
-      expect(1).toBe(1);
+    test("Test ejemplo", async () => {
+      const response = await request.get("/");
+      expect(response.statusCode).toBe(200);
     });
   });
 });
