7a6ffa003b0c8aae2ba22be84e06179525e248f0
Agus <80712756+agustintortir@users.noreply.github.com>
2022-06-30 15:55:57 -0300

    Prueba error 404 test deberia fallar

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
+  res.status(400).send("");
+  return;
   res.send("Hola mundo <br>" + "/*******  HASH_DATE *******/" + "_" + "/*******  HASH_ID *******/" )
 }); /* abc */
 app.listen(port, () => {
