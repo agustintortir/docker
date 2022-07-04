0262749c7d3eb7ce2fc37e7b5bf46b5d91b2686f
Agus <80712756+agustintortir@users.noreply.github.com>
2022-07-04 13:41:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send(404);
   res.send("Hola mundo <br>" + "/*******  HASH_DATE *******/" + "_" + "/*******  HASH_ID *******/" )
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
+//s
 module.exports = app;
